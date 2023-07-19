4fa972b8d4cc33482ffa4fe81d95dc6a28e6bd6a
alexFlorido <alexflorido0897@gmail.com>
2023-07-19 09:05:59 -0400

    [ADD] New Module

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "space_mission",
+    'summary': """Module to Logistic Handle of a Space Mission""",
+    'description': """Module Handle: 
+            - Logisctic
+            - Space travel ships
+            - Crew
+            """,
+    'license': "OPL-1",
+    'author': "alexFlorido",
+    'website': 'www.odoo.com',
+    'category': "Custom Modules/Space Mission",
+    "depends": ['base'] ,
+    'data': [],
+    'demo': [],
+    'application': True,
+}
